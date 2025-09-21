8e217c645d0e88e03f66c7e631000787ddd7deb1
Aryan Singh <137031264+gambare2@users.noreply.github.com>
2025-09-21 16:03:53 +0530

    Something left

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -78,8 +78,8 @@ export default function Home() {
     try {
       setLoading(true);
       const res = await axios.post(
-        // `${import.meta.env.VITE_API_URL}/api/admin/admin-login`,
-        "http://localhost:5000/api/admin/admin-login",
+        `${import.meta.env.VITE_API_URL}/api/admin/admin-login`,
+        // "http://localhost:5000/api/admin/admin-login",
         form,
         {
           withCredentials: true,
